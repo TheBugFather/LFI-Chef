8ab2fe24a523289588f7f0bcfb013e89ec89f836
Nicholas Gimbel <thebugfather@Nicholass-MacBook-Pro.local>
2023-08-09 10:06:33 -0400

    08/09/23 - Development patch

--- lfi_chef.py
+++ lfi_chef.py
@@ -5,6 +5,62 @@ import sys
 from pathlib import Path
 
 
+def main(config_obj):
+    line_buffer = ''
+    path_indexes = []
+
+    try:
+        # Open the input wordlist in read mode and output wordlist in append mode #
+        with config_obj.in_file.open('r', encoding='utf-8') as in_file, \
+             config_obj.out_file.open('a', encoding='utf-8') as out_file:
+            # Iterate through input wordlist line by line #
+            for line in in_file:
+                line_buffer += line
+                # Reset char counter #
+                char_count = 0
+                # Iterate through line char by char #
+                for char in line:
+                    # If the mode is Windows and char is backslash path
+                    # or the mode is Linux/Mac and char is slash path #
+                    if (config_obj.mode == 'Windows' and char == '\\') \
+                    or (config_obj.mode != 'Windows' and char == '/'):
+                        # Append current index in line to path indexes for char replacement #
+                        path_indexes.append(char_count)
+
+                    char_count += 1
+
+                # If there were path chars in file path to be parsed #
+                if path_indexes:
+                    # Iterate through the path char replace encodings #
+                    for slash_parse in config_obj.path_chars:
+                        # Iterate through the tracked indexes where slashes exist #
+                        for index in path_indexes:
+                            # If the wordlist mode is windows #
+                            if config_obj.mode == 'Windows':
+                                # Replace backslash with current parsing character #
+                                line_buffer[index].replace('\\', slash_parse)
+                            # If the wordlist mode is mac or linux #
+                            else:
+                                # Replace slash with current parsing character #
+                                line_buffer[index].replace('/', slash_parse)
+
+                        # Write new line with parsed path characters #
+                        out_file.write(line_buffer)
+
+                    # Reset path indexes list per line #
+                    path_indexes = []
+
+                # Reset line buffer #
+                line_buffer = ''
+
+    # If error occurs during file operation #
+    except OSError as file_err:
+        # Print error, log, and exit program #
+        print_err(f'Error occurred during file operation: {file_err}')
+        logging.error('Error occurred during file operation: %s', file_err)
+        sys.exit(3)
+
+
 def print_err(msg: str):
     """
     Prints error message through standard error.
@@ -14,10 +70,6 @@ def print_err(msg: str):
     """
     #  Print error via standard error #
     print(f'\n* [ERROR] {msg} *\n', file=sys.stderr)
-
-
-def main(config_obj):
-    pass
 
 
 class ProgramConfig:
@@ -45,7 +97,7 @@ class ProgramConfig:
         # If the passed in file path is a directory #
         if file_path.is_dir():
             # Print error and exit #
-            print_err('Passed in file path does not exist on file system')
+            print_err(f'The file {file_path.name} is a directory, not a file')
             sys.exit(2)
 
         # If the passed in file path is not absolute #
@@ -66,12 +118,12 @@ if __name__ == '__main__':
                                                      'process of LFI wordlist generation')
     arg_parser.add_argument('in_file', help='The path to input file or name of file'
                                             ' if in same directory')
-    arg_parser.add_argument('mode', choices=['linux', 'windows'], help='The mode of LFI wordlists '
-                                                                       'to generate based on OS')
+    arg_parser.add_argument('mode', choices=['mac', 'linux', 'windows'],
+                            help='The mode of LFI wordlists to generate based on OS')
     arg_parser.add_argument('--out_file', help='The path where the output file is written or '
                                                'name of file if in same directory')
-    arg_parser.add_argument('--path_chars', help='The set of characters to be used as alternatives '
-                                                 'to native file path slashes Ex: %2f,\\,/')
+    arg_parser.add_argument('--path_chars', help='The set of comma-separated characters to be used '
+                                                 'as alternatives to native file path slashes')
     parsed_args = arg_parser.parse_args()
 
     # Initialize program configuration class #
@@ -84,6 +136,8 @@ if __name__ == '__main__':
     # If an output file arg was specified #
     if parsed_args.out_file:
         conf_obj.out_file = conf_obj.validate_file(parsed_args.out_file)
+    else:
+        conf_obj.out_file = conf_obj.cwd / f'LFI-Chef_{conf_obj.mode}_wordlist.txt'
 
     # If path characters to be parsed into output paths were specified #
     if parsed_args.path_chars:
@@ -91,7 +145,7 @@ if __name__ == '__main__':
         conf_obj.path_chars = parsed_args.path_chars.split(',')
     # Otherwise use default char set #
     else:
-        conf_obj.path_chars = ['%2f']
+        conf_obj.path_chars = ['%2a', '%2b', '%2c', '%2d', '%2e', '%2f']
 
     # Setup the log file and logging facilities #
     logging.basicConfig(filename='LFI-Chef.log',
@@ -108,43 +162,3 @@ if __name__ == '__main__':
         RET = 1
 
     sys.exit(RET)
-
-
-
-
-
-# def error_query(err_path: str, err_mode: str, err_obj):
-#     """
-#     Looks up the errno message to get description.
-#
-#     :param err_path:  The path to the file where the error occurred.
-#     :param err_mode:  The file mode used during the error.
-#     :param err_obj:  The error message instance.
-#     :return:  Nothing
-#     """
-#     # If file does not exist #
-#     if err_obj.errno == errno.ENOENT:
-#         print_err(f'{err_path} does not exist')
-#         logging.error('%s does not exist', err_path)
-#
-#     # If the file does not have read/write access #
-#     elif err_obj.errno == errno.EPERM:
-#         print_err(f'{err_path} does not have permissions for {err_mode}'
-#                   ' file mode, if file exists confirm it is closed')
-#         logging.error('%s does not have permissions for %s file mode, if file '
-#                       'exists confirm it is closed', err_path, err_mode)
-#
-#     # File IO error occurred #
-#     elif err_obj.errno == errno.EIO:
-#         print_err(f'IO error occurred during {err_mode} mode on {err_path}')
-#         logging.error('IO error occurred during %s mode on %s', err_mode, err_path)
-#
-#     # If other unexpected file operation error occurs #
-#     else:
-#         print_err(f'Unexpected file operation error occurred accessing {err_path}: {err_obj.errno}')
-#         logging.error('Unexpected file operation error occurred accessing %s: %s',
-#                       err_path, err_obj.errno)
-
-
-
-
